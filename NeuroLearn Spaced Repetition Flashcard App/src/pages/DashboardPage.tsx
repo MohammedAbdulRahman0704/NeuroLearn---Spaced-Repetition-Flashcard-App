ee5bcdc48a217f0b158035cf362a1889cde7870c
Mohammed Abdul Rahman <abdul.rahman.190704@gmail.com>
2025-05-16 13:48:33 +0530

    Add DashboardPage component with simple header

---
+++ NeuroLearn Spaced Repetition Flashcard App/src/pages/DashboardPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardPage: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold text-gray-900 dark:text-white">Dashboard</h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
