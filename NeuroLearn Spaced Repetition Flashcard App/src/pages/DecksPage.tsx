dc561e219c88fbaed1091e14dc1691f6385a4859
Mohammed Abdul Rahman <abdul.rahman.190704@gmail.com>
2025-05-16 13:50:18 +0530

    Add DecksPage component showing user's decks header

---
+++ NeuroLearn Spaced Repetition Flashcard App/src/pages/DecksPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DecksPage: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold text-gray-900 dark:text-white">My Decks</h1>
+    </div>
+  );
+};
+
+export default DecksPage;
